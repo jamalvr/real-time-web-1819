f871e1734f9c3a12eb3dfaef243fa2dc81bd1f81
Jamal van Rooijen <jamal@vergelijkgroep.nl>
2020-02-21 17:02:21 +0100

    Client code to object literals

--- public/js/client.js
+++ public/js/client.js
@@ -11,171 +11,182 @@
         /////////
         ///////// Helper
         /////////
-        const kebabString = function (string) {
-            let kebabify = string.replace(/\s/g, '-');
-            let toLowerCase = kebabify.toLowerCase();
-            return toLowerCase;
-        };
+        const helper = {
+            kebabString: function (string) {
+                let kebabify = string.replace(/\s/g, '-');
+                let toLowerCase = kebabify.toLowerCase();
+                return toLowerCase;
+            }
+        }
 
         /////////
         //////// Client functionality
         /////////
         // Start game button
-        const startGame = function () {
-            const gameButton = document.querySelector('.game-start');
-            gameButton.classList.remove('hide');
+        const game = {
+            start: function () {
+                const gameButton = document.querySelector('.game-start');
+                gameButton.classList.remove('hide');
 
-            gameButton.addEventListener('click', function () {
-                socket.emit('startGame');
-            });
-        };
-
-        // Sets the client's username
-        const setUsername = function () {
-            const usernameForm = document.getElementById('username');
-            const usernameInput = document.querySelector('.username-input');
-            usernameForm.classList.remove('hide');
-
-            usernameForm.addEventListener('submit', function (event) {
-                // Prevent browser refresh
-                event.preventDefault();
-
-                // Get clean user input values
-                let usernameValue = usernameInput.value;
-                username = kebabString(usernameValue);
-
-                // Tell the server your username
-                socket.emit('username', username, function (callback) { // Hoe komt data van username nou hier? Dat snap ik niet helemaal
-                    if (callback) {
-                        usernameForm.classList.add('hide');
-                    } else {
-                        alert('Al bezet biiiiitch');
-                    }
+                gameButton.addEventListener('click', function () {
+                    socket.emit('startGame');
                 });
-            })
+            },
         };
 
-        // Update user list
-        const updateUserList = function (userList) {
-            const listElement = document.getElementById('user-list');
+        const user = {
+            // Sets the client's username
+            setUsername: function () {
+                const usernameForm = document.getElementById('username');
+                const usernameInput = document.querySelector('.username-input');
+                usernameForm.classList.remove('hide');
 
-            // Clear old html
-            listElement.innerHTML = '';
+                usernameForm.addEventListener('submit', function (event) {
+                    // Prevent browser refresh
+                    event.preventDefault();
 
-            userList.forEach(function (user) {
-                let template = `
-            <li class="user ${user.username}">
-                <span class="name">${user.username}</span>
-                <span class="score">Score: ${user.score}</span>
-            </li>`;
-                return listElement.innerHTML += template;
-            });
-        };
+                    // Get clean user input values
+                    let usernameValue = usernameInput.value;
+                    username = helper.kebabString(usernameValue);
 
-        // Show city input field and push to server
-        const getCity = function () {
-            const cityFormElement = document.getElementById('city');
-            const cityValueElement = document.querySelector('.city-input');
+                    // Tell the server your username
+                    socket.emit('username', username, function (callback) { // Hoe komt data van username nou hier? Dat snap ik niet helemaal
+                        if (callback) {
+                            usernameForm.classList.add('hide');
+                        } else {
+                            alert('Al bezet biiiiitch');
+                        }
+                    });
+                })
+            },
 
-            // Show input field
-            cityFormElement.classList.remove('hide');
-            console.log('city input is shown')
+            // Update user list
+            addToList: function (userList) {
+                const listElement = document.getElementById('user-list');
 
-            // Add submit to communicate to server
-            cityFormElement.addEventListener('submit', function (event) {
-                // Prevent browser refresh
-                event.preventDefault();
+                // Clear old html
+                listElement.innerHTML = '';
 
-                // Get value from user
-                let cityValue = cityValueElement.value;
-                console.log('emitting your city: ' + cityValue);
-
-                // Send input value back to the server
-                socket.emit('cityValue', cityValue, function (callback) {
-                    console.log(callback);
-                    if (callback) {
-                        cityFormElement.classList.add('hide');
-                    }
+                userList.forEach(function (user) {
+                    let template = `
+                    <li class="user ${user.username}">
+                        <span class="name">${user.username}</span>
+                        <span class="score">Score: ${user.score}</span>
+                    </li>`;
+                    return listElement.innerHTML += template;
                 });
-            });
-        };
-
-        // Template to show the chosen city to user
-        const showCity = function (currentCity) {
-            let cityContainer = document.getElementById('current-city');
-            let template = `<h2 class="city-name">Wat is het weer in ${currentCity}?</h2>`;
-
-            return cityContainer.innerHTML = template;
-        }
-
-        const showAnswers = function (answers) {
-            // Get static parent HTML to place answers in
-            let answerList = document.getElementById('current-answers');
-            answerList.innerHTML = '';
-
-            answers.forEach(function (answer) {
-                // Create nodes
-                let listItem = document.createElement('li');
-                let button = document.createElement('button');
-                let text = document.createTextNode(answer);
-
-                // Add stuff to node(s)
-                button.value = answer;
-                button.classList.add('answer');
-
-                // Append nodes to dom
-                listItem.appendChild(button);
-                button.appendChild(text);
-                answerList.appendChild(listItem);
-
-                // Event listener
-                button.addEventListener('click', function () {
-                    let userAnswer = button.value;
-                    userAnswer.toString();
-                    console.log('Answering question with:', userAnswer);
-
-                    // Send answer back to server
-                    socket.emit('userAnswer', userAnswer);
-                });
-            });
-        }
-
-        const getAllScores = function (userList) {
-            let playerScore = 0;
-            let allScores = [];
-
-            // Get user specific score
-            userList.forEach(function (user) {
-                if (user.username === username) {
-                    playerScore = user.score;
-                }
-            });
-
-            // map scores
-            userList.map(function score(user) {
-                let score = user.score
-                return allScores.push(score);
-            });
-
-            showWinner(playerScore, allScores);
-        }
-
-        const showWinner = function (playerScore, allScores) {
-            let winnerWinner = document.getElementById('chicken-dinner');
-            let template = null;
-
-            winnerWinner.classList.remove('hide');
-
-            // Get highest score
-            let highestScore = Math.max(...allScores);
-
-            if (playerScore === highestScore) {
-                template = `<h1>You win!</h1>`
-            } else {
-                template = `<h1>You lose!</h1>`
             }
+        };
 
-            winnerWinner.innerHTML = template;
+        const city = {
+            // Show city input field and push to server
+            get: function () {
+                const cityFormElement = document.getElementById('city');
+                const cityValueElement = document.querySelector('.city-input');
+
+                // Show input field
+                cityFormElement.classList.remove('hide');
+                console.log('city input is shown')
+
+                // Add submit to communicate to server
+                cityFormElement.addEventListener('submit', function (event) {
+                    // Prevent browser refresh
+                    event.preventDefault();
+
+                    // Get value from user
+                    let cityValue = cityValueElement.value;
+                    console.log('emitting your city: ' + cityValue);
+
+                    // Send input value back to the server
+                    socket.emit('cityValue', cityValue, function (callback) {
+                        console.log(callback);
+                        if (callback) {
+                            cityFormElement.classList.add('hide');
+                        }
+                    });
+                });
+            },
+
+            // Template to show the chosen city to user
+            show: function (currentCity) {
+                let cityContainer = document.getElementById('current-city');
+                let template = `<h2 class="city-name">Wat is het weer in ${currentCity}?</h2>`;
+
+                return cityContainer.innerHTML = template;
+            },
+
+            showAnswers: function (answers) {
+                // Get static parent HTML to place answers in
+                let answerList = document.getElementById('current-answers');
+                answerList.innerHTML = '';
+
+                answers.forEach(function (answer) {
+                    // Create nodes
+                    let listItem = document.createElement('li');
+                    let button = document.createElement('button');
+                    let text = document.createTextNode(answer);
+
+                    // Add stuff to node(s)
+                    button.value = answer;
+                    button.classList.add('answer');
+
+                    // Append nodes to dom
+                    listItem.appendChild(button);
+                    button.appendChild(text);
+                    answerList.appendChild(listItem);
+
+                    // Event listener
+                    button.addEventListener('click', function () {
+                        let userAnswer = button.value;
+                        userAnswer.toString();
+                        console.log('Answering question with:', userAnswer);
+
+                        // Send answer back to server
+                        socket.emit('userAnswer', userAnswer);
+                    });
+                });
+            }
+        };
+
+        const score = {
+            getAll: function (userList) {
+                let playerScore = 0;
+                let allScores = [];
+
+                // Get user specific score
+                userList.forEach(function (user) {
+                    if (user.username === username) {
+                        playerScore = user.score;
+                    }
+                });
+
+                // map scores
+                userList.map(function score(user) {
+                    let score = user.score
+                    return allScores.push(score);
+                });
+
+                score.showWinner(playerScore, allScores);
+            },
+
+            showWinner: function (playerScore, allScores) {
+                let winnerWinner = document.getElementById('chicken-dinner');
+                let template = null;
+
+                winnerWinner.classList.remove('hide');
+
+                // Get highest score
+                let highestScore = Math.max(...allScores);
+
+                // If player has highest score and is not zero, wins!
+                if (playerScore === highestScore && playerScore !== 0) {
+                    template = `<h1>You win!</h1>`
+                } else {
+                    template = `<h1>You lose!</h1>`
+                }
+
+                winnerWinner.innerHTML = template;
+            }
         }
 
         // Cheat sheet menu
@@ -197,8 +208,6 @@
             },
         };
 
-        cheatSheet.button();
-
         /////////
         ///////// Socket listeners
         /////////
@@ -206,17 +215,17 @@
             // Check if game is already running when a new user/socket joins
             if (!gameRunning) {
                 // Enable socket to fill in his name
-                setUsername();
+                user.setUsername();
             }
         });
 
         // When server gets new users, push them to frontend
         socket.on('pushUserList', function (userList) {
-            updateUserList(userList);
+            user.addToList(userList);
 
             // Enable to start game after 'x' amount of users
             if (userList.length > 0 && !gameState) {
-                startGame();
+                game.start();
             }
         });
 
@@ -231,25 +240,26 @@
 
         socket.on('cityNotFound', function (cityValue) {
             window.alert('Hey, dude... ' + cityValue + ' bestaat toch helemaal niet man');
-            getCity();
+            city.get();
         });
 
         // Server calls that the user can type a city to their liking
         socket.on('inputCity', function () {
-            getCity();
+            city.get();
         });
 
         // A (new) question comes in from the server with the chosen city and possible answers
         socket.on('newQuestion', function (currentCity, answers, correctAnswer) {
             // Show the current city to user
-            showCity(currentCity);
-            showAnswers(answers);
+            city.show(currentCity);
+            city.showAnswers(answers);
 
             // Push correctAnswer to the cheat sheet
+            cheatSheet.button();
             cheatSheet.container(correctAnswer);
         });
 
         socket.on('gameOver', function (userScore, userList) {
-            getAllScores(userScore, userList);
+            score.getAll(userScore, userList);
         });
     })();
