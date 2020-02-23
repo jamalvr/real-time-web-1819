e2ef07b2e885d426f25fdc5ca65883528bcae00a
Jamal van Rooijen <jamal@vergelijkgroep.nl>
2020-02-24 00:03:09 +0100

    Heroku port addded

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-const port = 3000;
+const port = process.env.PORT || 3000;
 const request = require('request');
 
 ////// Set engine & directories
